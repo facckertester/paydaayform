3c8a7f59df180f07e277ecb958a40cf4ec51d193
facckertester <winnyhill@mail.ru>
2025-06-10 20:58:03 +0300

    Add files via upload

---
+++ achievements.js
@@ -0,0 +1,5 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.getElementById("achievements");
+  container.innerHTML = "<p>Здесь будет список достижений (данные загружаются позже).</p>";
+});
