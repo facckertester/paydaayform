383c93467f46317408642395b163df57729780ca
facckertester <winnyhill@mail.ru>
2025-06-10 21:31:04 +0300

    Delete achievements.js

--- achievements.js
+++
@@ -1,5 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", () => {
-  const container = document.getElementById("achievements");
-  container.innerHTML = "<p>Здесь будет список достижений (данные загружаются позже).</p>";
-});
